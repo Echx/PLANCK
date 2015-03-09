07ba279d92433dfeff24b4097aae37a7057c8655
Lei Mingyu <uygnim.iel@gmail.com>
2015-03-10 00:29:27 +0800

    base sprites added

---
+++ Echx/Echx/XPhoton.swift
@@ -0,0 +1,20 @@
+//
+//  XPhoton.swift
+//  Echx
+//
+//  Created by Lei Mingyu on 10/03/15.
+//  Copyright (c) 2015年 Echx. All rights reserved.
+//
+
+import UIKit
+
+class XPhoton {
+    var illuminance: CGFloat
+    var color: XColor
+    
+    init() {
+        illuminance = 0.0
+        color = XColor()
+    }
+}
+
