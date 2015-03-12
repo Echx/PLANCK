8a1e6a9082d00778ccc6c67be5af4f1949399c57
Wang Jinghan <phantom.there@gmail.com>
2015-03-12 23:48:16 +0800

    update scene to test

--- Planck/Planck/Views/GameScene.swift
+++ Planck/Planck/Views/GameScene.swift
@@ -11,35 +11,69 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        let myLabel = SKLabelNode(fontNamed:"Avenir-Medium")
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
     }
-    
+
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let sprite = XPhoton(illuminance: 0, color: XColor(), direction: CGVector(dx: 1, dy: 1))
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            if sprite.parent != nil {
+                sprite.removeActionForKey(ActionKey.photonActionLinear)
+                let direction = CGVector(dx: -sprite.direction.dx, dy: sprite.direction.dy)
+                sprite.setDirection(direction)
+                sprite.runAction(SKAction.repeatActionForever(sprite.getAction()), withKey: ActionKey.photonActionLinear)
+            } else {
+                sprite.position = location
+                sprite.runAction(SKAction.repeatActionForever(sprite.getAction()), withKey: ActionKey.photonActionLinear)
+                self.addChild(sprite)
+            }
+
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        self.enumerateChildNodesWithName(NodeName.xPhoton, usingBlock:  {
+            (node: SKNode!, stop: UnsafeMutablePointer <ObjCBool>) -> Void in
+            var sprite = node as XPhoton
+            if sprite.parent != nil {
+                if sprite.position.x < 0 || sprite.position.x > 1024 {
+                    sprite.removeActionForKey(ActionKey.photonActionLinear)
+                    let direction = CGVector(dx: -sprite.direction.dx, dy: sprite.direction.dy)
+                    sprite.setDirection(direction)
+                    var position = sprite.position;
+                    if position.x < 0 {
+                        position.x = 0
+                    } else {
+                        position.x = 1024
+                    }
+                    sprite.position = position
+                    sprite.runAction(SKAction.repeatActionForever(sprite.getAction()), withKey: ActionKey.photonActionLinear)
+                }
+                
+                if sprite.position.y < 0 || sprite.position.y > 768 {
+                    sprite.removeActionForKey(ActionKey.photonActionLinear)
+                    let direction = CGVector(dx: sprite.direction.dx, dy: -sprite.direction.dy)
+                    sprite.setDirection(direction)
+                    var position = sprite.position;
+                    if position.y < 0 {
+                        position.y = 0
+                    } else {
+                        position.y = 768
+                    }
+                    sprite.position = position
+                    sprite.runAction(SKAction.repeatActionForever(sprite.getAction()), withKey: ActionKey.photonActionLinear)
+                }
+            }
+        })
     }
 }
