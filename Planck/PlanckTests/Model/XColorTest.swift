5156a337cd9ba39368ebcc672107bc0bef4975e6
Jiang Sheng <gisonrg@gmail.com>
2015-03-13 23:22:31 +0800

    Add unit test for xcolor.

--- Planck/PlanckTests/Model/XColorTest.swift
+++ Planck/PlanckTests/Model/XColorTest.swift
@@ -14,6 +14,11 @@ class XColorTest: XCTestCase {
     let pureRed = XColor(containsRed: true, containsGreen: false, containsBlue: false)
     let pureGreen = XColor(containsRed: false, containsGreen: true, containsBlue: false)
     let pureBlue = XColor(containsRed: false, containsGreen: false, containsBlue: true)
+    let pureYellow = XColor(containsRed: true, containsGreen: true, containsBlue: false)
+    let pureCyan = XColor(containsRed: false, containsGreen: true, containsBlue: true)
+    let pureMagenta = XColor(containsRed: true, containsGreen: false, containsBlue: true)
+    
+    let pureWhite = XColor(containsRed: true, containsGreen: true, containsBlue: true)
     
     func testRed() {
         let redColor1 = XColor(containsRed: true, containsGreen: false, containsBlue: false)
@@ -42,18 +47,93 @@ class XColorTest: XCTestCase {
     }
     
     func testYellow() {
-        let yellowColor = XColor(containsRed: true, containsGreen: false, containsBlue: false)
+        let greenColor = XColor(containsRed: false, containsGreen: true, containsBlue: false)
+        let redColor = XColor(containsRed: true, containsGreen: false, containsBlue: false)
+        
+        let resultedYellow = redColor + greenColor
+        
+        XCTAssertEqual(resultedYellow, pureYellow,
+                        "green + red should be yellow!")
+        
+        XCTAssertEqual(resultedYellow + resultedYellow, pureYellow,
+                        "yellow + yellow should be yellow!")
+        
+        XCTAssertEqual(pureYellow + pureRed, pureYellow,
+                        "yellow + red should be yellow!")
+        XCTAssertEqual(pureYellow + pureGreen, pureYellow,
+                        "yellow + green should be yellow!")
     }
     
     func testCyan() {
-        let cyanColor = XColor(containsRed: true, containsGreen: false, containsBlue: false)
+        let greenColor = XColor(containsRed: false, containsGreen: true, containsBlue: false)
+        let blueColor = XColor(containsRed: false, containsGreen: false, containsBlue: true)
+        
+        let resultedCyan = greenColor + blueColor
+        
+        XCTAssertEqual(resultedCyan, pureCyan,
+                        "green + blue should be cyan!")
+        
+        XCTAssertEqual(resultedCyan + resultedCyan, pureCyan,
+                        "cyan + cyan should be cyan!")
+        
+        XCTAssertEqual(pureBlue + pureCyan, pureCyan,
+                        "cyan + blue should be cyan!")
+
+        XCTAssertEqual(pureCyan + pureGreen, pureCyan,
+                        "cyan + green should be cyan!")
     }
     
     func testMagenta() {
-        let magentaColor = XColor(containsRed: true, containsGreen: false, containsBlue: false)
+        let redColor = XColor(containsRed: true, containsGreen: false, containsBlue: false)
+        let blueColor = XColor(containsRed: false, containsGreen: false, containsBlue: true)
+        
+        let resultedMagenta = redColor + blueColor
+        
+        XCTAssertEqual(resultedMagenta, pureMagenta,
+                        "red + blue should be Magenta!")
+        
+        XCTAssertEqual(resultedMagenta + resultedMagenta, pureMagenta,
+                        "Magenta + Magenta should be Magenta!")
+        
+        XCTAssertEqual(pureBlue + pureMagenta, pureMagenta,
+                        "Magenta + blue should be Magenta!")
+        
+        XCTAssertEqual(pureRed + pureMagenta, pureMagenta,
+                        "Magenta + green should be Magenta!")
     }
     
     func testWhite() {
-        let whiteColor = XColor(containsRed: true, containsGreen: false, containsBlue: false)
+        XCTAssertEqual(pureRed + pureCyan, pureWhite,
+            "red + cyan should be white!")
+        XCTAssertEqual(pureRed + pureWhite, pureWhite,
+            "red + white should be white!")
+        
+        XCTAssertEqual(pureGreen + pureMagenta, pureWhite,
+            "green + magenta should be white!")
+        XCTAssertEqual(pureGreen + pureWhite, pureWhite,
+            "green + white should be white!")
+        
+        XCTAssertEqual(pureBlue + pureYellow, pureWhite,
+            "blue + yellow should be white!")
+        XCTAssertEqual(pureBlue + pureWhite, pureWhite,
+            "blue + white should be white!")
+        
+        XCTAssertEqual(pureCyan + pureMagenta, pureWhite,
+            "cyan + magenta should be white!")
+        XCTAssertEqual(pureCyan + pureYellow, pureWhite,
+            "cyan + yellow should be white!")
+        XCTAssertEqual(pureCyan + pureWhite, pureWhite,
+            "cyan + white should be white!")
+        
+        XCTAssertEqual(pureMagenta + pureYellow, pureWhite,
+            "magenta + yellow should be white!")
+        XCTAssertEqual(pureMagenta + pureWhite, pureWhite,
+            "magenta + white should be white!")
+        
+        XCTAssertEqual(pureYellow + pureWhite, pureWhite,
+            "yellow + white should be white!")
+        
+        XCTAssertEqual(pureWhite + pureWhite, pureWhite,
+            "white + white should be white!")
     }
 }
