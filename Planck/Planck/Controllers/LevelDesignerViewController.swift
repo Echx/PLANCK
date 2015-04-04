5fc4a0d09e2b1129ee1fdf05f5cce970c6e76491
Wang Jinghan <phantom.there@gmail.com>
2015-04-04 20:01:54 +0800

    Gesture Recognizer

--- Planck/Planck/Controllers/LevelDesignerViewController.swift
+++ Planck/Planck/Controllers/LevelDesignerViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class LevelDesignerViewController: UIViewController {
 
+    @IBOutlet var deviceSegment: UISegmentedControl?
+    @IBOutlet var inputModeSegment: UISegmentedControl?
+    
+    struct Selectors {
+        static let tapAction: Selector = "viewDidTapped:"
+        static let panAction: Selector = "viewDidPanned:"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setUpGestureRecognizers()
+    }
+    
+    private func setUpGestureRecognizers() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selectors.tapAction)
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: Selectors.panAction)
+        self.view.addGestureRecognizer(tapGestureRecognizer)
     }
 
+    
+    //MARK - tap gesture handler
+    func viewDidTapped(sender: UITapGestureRecognizer) {
+        
+    }
+    
+    //MARK - pan gesture handler
+    func viewDidPanned(sender: UIPanGestureRecognizer) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
