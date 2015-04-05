b49ad012f47adaeda32b228ece8539a5d4453f05
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-05 22:33:24 +0800

    so long our sprites

--- Planck/Planck/Models/Optics/XFlatMirror.swift
+++ Planck/Planck/Models/Optics/XFlatMirror.swift
@@ -1,9 +1,15 @@
 //
-//  XMirror.swift
+//  XFlatMirror.swift
 //  Planck
 //
 //  Created by Lei Mingyu on 05/04/15.
-//  Copyright (c) 2015年 Echx. All rights reserved.
+//  Copyright (c) 2015 Echx. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class XFlatMirror: GOFlatMirrorRep {
+    override init(center: GOCoordinate, thickness: CGFloat, length: CGFloat, direction: CGVector, id: String) {
+        super.init(center: center, thickness: thickness, length: length, direction: direction, id: id)
+    }
+}
