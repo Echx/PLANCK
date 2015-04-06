7dda5c81b489e1bb0038bd4486be3a567347b8a4
Wang Jinghan <phantom.there@gmail.com>
2015-04-07 02:25:45 +0800

    Add Controllers

---
+++ Planck/Planck/Controllers/LevelSelectorViewDesigner.swift
@@ -0,0 +1,13 @@
+//
+//  LevelSelectorViewDesigner.swift
+//  Planck
+//
+//  Created by NULL on 07/04/15.
+//  Copyright (c) 2015年 Echx. All rights reserved.
+//
+
+import UIKit
+
+class LevelSelectorViewDesigner: UIViewController {
+
+}
---
+++ Planck/Planck/Controllers/SettingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.swift
+//  Planck
+//
+//  Created by NULL on 07/04/15.
+//  Copyright (c) 2015年 Echx. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+}
