bc1d5e8da8ebb25fa00410f03d466df9e4a62724
Jiang Sheng <gisonrg@gmail.com>
2015-04-11 00:17:13 +0800

    Add NSCoding for each XNode class.

--- Planck/Planck/Models/Optics/XFlatWall.swift
+++ Planck/Planck/Models/Optics/XFlatWall.swift
@@ -15,7 +15,17 @@ class XFlatWall: XNode {
     }
 
     required convenience init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        let body = aDecoder.decodeObjectForKey("phyBody") as GOFlatWallRep
+        let isPlanck = aDecoder.decodeBoolForKey("isPlanck")
+        let shouldPlaySould = aDecoder.decodeBoolForKey("shouldPlaySound")
+        self.init(flatWall: body)
     }
+    
+    override func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.physicsBody, forKey: "phyBody")
+        aCoder.encodeBool(self.isPlanck, forKey: "isPlanck")
+        aCoder.encodeBool(self.shouldPlaySound, forKey: "shouldPlaySound")
+    }
+
 }
 
