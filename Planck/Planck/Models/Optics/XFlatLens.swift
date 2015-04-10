a6d82633494182b0a50a0fbd1cf1b8e49d3638ff
Wang Jinghan <phantom.there@gmail.com>
2015-04-10 22:16:05 +0800

    add XFlatLens

---
+++ Planck/Planck/Models/Optics/XFlatLens.swift
@@ -0,0 +1,13 @@
+//
+//  XFlatLens.swift
+//  Planck
+//
+//  Created by Wang Jinghan on 10/04/15.
+//  Copyright (c) 2015 Echx. All rights reserved.
+//
+
+import UIKit
+
+class XFlatLens: GOFlatLensRep {
+    
+}
