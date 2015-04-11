4aecb271109629bc24a1e59e9ee2ab9d0b2b3cac
Jiang Sheng <gisonrg@gmail.com>
2015-04-12 01:24:18 +0800

    Set up for settingVC

--- Planck/Planck/Controllers/SettingViewController.swift
+++ Planck/Planck/Controllers/SettingViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class SettingViewController: XViewController, UITableViewDataSource, UITableViewDelegate {
     
+    private let settingCellID = "SettingViewCell"
+    private let textCellId = "SettingViewTextCell"
+    
+    private let numOfExtraSection = 2
+    
+    private let sectionIDForSupport = 3
+    private let sectionIDForGameCenter = 4
+    
     class func getInstance() -> SettingViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let identifier = StoryboardIndentifier.Setting
@@ -28,14 +36,54 @@ class SettingViewController: XViewController, UITableViewDataSource, UITableView
         // Dispose of any resources that can be recreated.
     }
     
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return getStaticTogglableItems().count + numOfExtraSection // setting items + support + game center
     }
-    */
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.section == sectionIDForSupport {
+            let cell = tableView.dequeueReusableCellWithIdentifier(textCellId, forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = getStaticSupportItems()[indexPath.item]
+            return cell
+        } else if indexPath.section == sectionIDForGameCenter {
+            let cell = tableView.dequeueReusableCellWithIdentifier(textCellId, forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = getStaticGameCenterSupportItems()[indexPath.item]
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier(settingCellID, forIndexPath: indexPath) as SettingViewCell
+            cell.title.text = getStaticTogglableItems()[indexPath.section][indexPath.item]
+            return cell
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == sectionIDForSupport {
+            return getStaticSupportItems().count
+        } else if section == sectionIDForGameCenter {
+            return getStaticGameCenterSupportItems().count
+        } else {
+            return getStaticTogglableItems()[section].count
+        }
+    }
+
+    
+    private func getStaticTogglableItems() -> [[String]] {
+        var cellItems = [
+            ["background music"], // audio setting
+            ["free rotate"],      // control setting
+            ["visual effects"]    // video setting
+        ]
+        return cellItems
+    }
+    
+    private func getStaticGameCenterSupportItems() -> [String] {
+        var cellItems = ["achievements", "leaderboards"]
+        return cellItems
+    }
+    
+    private func getStaticSupportItems() -> [String] {
+        var cellItems = ["rate us", "credits", "feedbacl"]
+        return cellItems
+    }
 }
