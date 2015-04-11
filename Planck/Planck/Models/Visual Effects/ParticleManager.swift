b9ed5a70c4f9f59cd156335cde773ba05a27d144
Wang Jinghan <phantom.there@gmail.com>
2015-04-12 03:58:45 +0800

    file location changed

---
+++ Planck/Planck/Models/Visual Effects/ParticleManager.swift
@@ -0,0 +1,13 @@
+//
+//  ParticleManager.swift
+//  Planck
+//
+//  Created by NULL on 12/04/15.
+//  Copyright (c) 2015年 Echx. All rights reserved.
+//
+
+import UIKit
+
+class ParticleManager: NSObject {
+   
+}
