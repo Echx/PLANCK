8490fdeac874af9fcf431c414796b75c3f9d10da
Jiang Sheng <gisonrg@gmail.com>
2015-04-12 00:51:11 +0800

    Add setting view cell

---
+++ Planck/Planck/Views/Setting/SettingViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewCell.swift
+//  Planck
+//
+//  Created by Jiang Sheng on 12/4/15.
+//  Copyright (c) 2015 Echx. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var toggle: UISwitch!
+}
