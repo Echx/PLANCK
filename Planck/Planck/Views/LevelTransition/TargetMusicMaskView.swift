c034f691522c600420b23bb1e38799349139576e
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-16 02:00:48 +0800

    update music mask

--- Planck/Planck/Views/LevelTransition/TargetMusicMaskView.swift
+++ Planck/Planck/Views/LevelTransition/TargetMusicMaskView.swift
@@ -14,6 +14,7 @@ protocol TargetMusicMaskViewDelegate {
 
 class TargetMusicMaskView: UIView {
     private let headphoneImage = UIImage(named: "headphone")
+    private var audioPlayer: AVAudioPlayer?
     var delegate: TargetMusicMaskViewDelegate?
     
     override init() {
@@ -24,11 +25,16 @@ class TargetMusicMaskView: UIView {
         self.addSubview(blurView)
         
         let headphoneView = UIImageView(image: self.headphoneImage)
+        headphoneView.frame = CGRect(x: 452, y: 324, width: 120, height: 120)
         self.addSubview(headphoneView)
     }
     
     
     func show(targetMusic: XMusic) {
+//        self.audioPlayer = AVAudioPlayer(contentsOfURL: SoundFiles.levelUpSound, error: nil)
+//        self.audioPlayer!.prepareToPlay()
+//        self.audioPlayer!.play()
+        
         
     }
     
