795892b2523d06e119a191ca5ae58b9ce913109a
Jiang Sheng <gisonrg@gmail.com>
2015-04-16 05:06:28 +0800

    Reload original level for level loader.

--- Planck/Planck/Controllers/DesignerLevelSelectViewController.swift
+++ Planck/Planck/Controllers/DesignerLevelSelectViewController.swift
@@ -98,26 +98,8 @@ class DesignerLevelSelectViewController: UITableViewController {
     
 
     private func loadFiles() {
-//        // find out the document path
-//        let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
-//            .UserDomainMask, true)[0] as NSString
-//        let fileManager = NSFileManager.defaultManager()
-//        let fileArray = fileManager.contentsOfDirectoryAtPath(path,
-//            error: nil)! as NSArray
-//        
-//        var levelFileLoader = StorageManager.defaultManager
-//        // iterate each filename to add
-//        for filename in fileArray {
-//            if ((filename.pathExtension) != nil) {
-//                if (filename.pathExtension == levelDataFileType) {
-//                    let game = levelFileLoader.loadLevel(filename as NSString)
-//                    levelArray.append(game)
-//                }
-//            }
-//        }
-//        
-//        // sort the levelArray based on Index
-//        levelArray.sort{$0<$1}
+        // make sure read the original level
+        StorageManager.defaultManager.setNeedsReload()
         self.levelArray = StorageManager.defaultManager.loadAllLevel()
     }
 }
