9eaab28577d01ff3f074082835354099cca484d7
Jiang Sheng <gisonrg@gmail.com>
2015-04-16 02:47:17 +0800

    Add view to moveable object.

--- Planck/Planck/Controllers/GameViewController.swift
+++ Planck/Planck/Controllers/GameViewController.swift
@@ -415,6 +415,13 @@ class GameViewController: XViewController {
         view.backgroundColor = UIColor.clearColor()
         view.layer.addSublayer(layer)
         self.deviceViews[node.id] = view
+        if let xnode = self.xNodes[node.id] {
+            if !xnode.isFixed {
+                let img = UIImageView(image: UIImage(named: "moveable"))
+                view.addSubview(img)
+                img.center = view.center
+            }
+        }
         self.view.insertSubview(view, atIndex: 0)
         
         var offsetX = CGFloat(coordinateBackup.x - node.center.x) * self.grid.unitLength
@@ -529,6 +536,7 @@ extension GameViewController: LevelTransitionMaskViewDelegate {
             if self.shouldShowNextLevel {
                 // save current game if it is not preview mode
                 if !isPreview {
+                    println("saving!")
                     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                         StorageManager.defaultManager.saveCurrentLevel(self.originalLevel)
                     })
@@ -538,6 +546,7 @@ extension GameViewController: LevelTransitionMaskViewDelegate {
                     // in preview mode, can neve reach here
                     // unlock next level and save it
                     nextLevel.isUnlock = true
+                    println("saving next!")
                     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
                         StorageManager.defaultManager.saveCurrentLevel(nextLevel)
                     })
