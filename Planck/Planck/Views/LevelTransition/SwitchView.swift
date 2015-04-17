02c506e127a312aa3dd6c947f80be8d5113c7967
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-17 22:36:23 +0800

    refactor the constants in switch view

--- Planck/Planck/Views/LevelTransition/SwitchView.swift
+++ Planck/Planck/Views/LevelTransition/SwitchView.swift
@@ -13,22 +13,25 @@ class SwitchView: UIView {
     let leftPole = CAShapeLayer()
     let rightPole = CAShapeLayer()
     
+    let lineWidth = CGFloat(3.0) // to be refactored
+    let circleRadius = CGFloat(6.0) // to be refactored
+    
     override init() {
         self.switchThrow.strokeColor = UIColor.whiteColor().CGColor
-        self.switchThrow.lineWidth = 5.0
+        self.switchThrow.lineWidth = lineWidth
         
         self.leftPole.strokeColor = UIColor.whiteColor().CGColor
-        self.leftPole.lineWidth = 5.0
+        self.leftPole.lineWidth = lineWidth
         
         self.rightPole.strokeColor = UIColor.whiteColor().CGColor
-        self.rightPole.lineWidth = 5.0
+        self.rightPole.lineWidth = lineWidth
         
         var throwPath = UIBezierPath()
 
         throwPath.lineJoinStyle = kCGLineJoinBevel
         throwPath.lineCapStyle = kCGLineCapRound
-        throwPath.moveToPoint(CGPoint(x: 40, y: 20))
-        throwPath.addLineToPoint(CGPoint(x: 90, y: 0))
+        throwPath.moveToPoint(CGPoint(x: 20 + 2 * circleRadius, y: 20))
+        throwPath.addLineToPoint(CGPoint(x: 40 + 2 * circleRadius, y: 0))
         
         var leftPolePath = UIBezierPath()
         
@@ -36,14 +39,14 @@ class SwitchView: UIView {
         leftPolePath.lineCapStyle = kCGLineCapRound
         leftPolePath.moveToPoint(CGPoint(x: 0, y: 20))
         leftPolePath.addLineToPoint(CGPoint(x: 20, y: 20))
-        leftPolePath.addArcWithCenter(CGPoint(x: 30, y: 20), radius: CGFloat(10), startAngle: CGFloat(-M_PI), endAngle: CGFloat(M_PI), clockwise: true)
+        leftPolePath.addArcWithCenter(CGPoint(x: 20 + circleRadius, y: 20), radius: CGFloat(circleRadius), startAngle: CGFloat(-M_PI), endAngle: CGFloat(M_PI), clockwise: true)
         
         var rightPolePath = UIBezierPath()
         
         rightPolePath.lineJoinStyle = kCGLineJoinBevel
         rightPolePath.lineCapStyle = kCGLineCapRound
-        rightPolePath.addArcWithCenter(CGPoint(x: 105, y: 20), radius: CGFloat(10), startAngle: CGFloat(0), endAngle: CGFloat(2 * M_PI), clockwise: true)
-        rightPolePath.addLineToPoint(CGPoint(x: 135, y: 20))
+        rightPolePath.addArcWithCenter(CGPoint(x: 85 + 2 * circleRadius, y: 20), radius: CGFloat(circleRadius), startAngle: CGFloat(0), endAngle: CGFloat(2 * M_PI), clockwise: true)
+        rightPolePath.addLineToPoint(CGPoint(x: 95 + 4 * circleRadius, y: 20))
 
         
         
