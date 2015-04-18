64a1b42b8339e871ea925bdcadb7b6706934b142
Wang Jinghan <phantom.there@gmail.com>
2015-04-19 04:08:52 +0800

    create OnboardingMaskView, change location of SwitchView

---
+++ Planck/Planck/Views/GamePlay/OnboardingMaskView.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingMaskView.swift
+//  Planck
+//
+//  Created by NULL on 19/04/15.
+//  Copyright (c) 2015年 Echx. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingMaskView: UIView {
+
+}
