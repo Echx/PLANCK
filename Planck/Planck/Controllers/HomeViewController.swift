b2e20b278470831ef1cab8f0e0ec3ce949d339f9
Wang Jinghan <phantom.there@gmail.com>
2015-04-25 20:21:40 +0800

    home view controller refactor

--- Planck/Planck/Controllers/HomeViewController.swift
+++ Planck/Planck/Controllers/HomeViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 import AVFoundation
 
+
+//This class controls the home view which will be presented when player first get into the game
 class HomeViewController: XViewController {
-    private let emitterView = UIView(frame: UIScreen.mainScreen().bounds)
-    private let backgroundMusicPlayer = AVAudioPlayer(contentsOfURL: SoundFiles.backgroundMusic, error: nil)
+    private let backgroundMusicPlayer = AVAudioPlayer(
+        contentsOfURL: SoundFiles.backgroundMusic, error: nil
+    )
     
     struct Selectors {
         static let stopMusic: Selector = "stopPlayingMusic:"
@@ -22,19 +25,20 @@ class HomeViewController: XViewController {
         static let min: Float = 0.05
         static let max: Float = 1
         static let fadeStep: Float = 0.05
+        static let initial: Float = 0
     }
     
     class func getInstance() -> HomeViewController {
-        let storyboard = UIStoryboard(name: StoryboardIdentifier.StoryBoardID, bundle: nil)
+        let storyboard = UIStoryboard(
+            name: StoryboardIdentifier.StoryBoardID, bundle: nil)
         let identifier = StoryboardIdentifier.Home
-        let viewController = storyboard.instantiateViewControllerWithIdentifier(identifier) as HomeViewController
+        let viewController = storyboard.instantiateViewControllerWithIdentifier(
+            identifier) as HomeViewController
         return viewController
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.insertSubview(self.emitterView, atIndex: 1)
-        self.emitterView.alpha = 0
         
         self.playMusic()
         
@@ -59,7 +63,7 @@ class HomeViewController: XViewController {
                 
                 dispatch_async(dispatch_get_main_queue(), {
                     GamiCent.reportAchievements(
-                        percent: 100.0,
+                        percent: 100.0, //hundred percentage
                         achievementID: XGameCenter.achi_newbie,
                         isShowBanner: true,
                         completion: nil)
@@ -77,52 +81,70 @@ class HomeViewController: XViewController {
     }
 
 
-    
+    //open right(game) menu
     @IBAction func playGame(sender: AnyObject?) {
-        self.getDrawerController()!.openDrawerSide(MMDrawerSide.Right,
-                                                    animated: true, completion: nil)
+        self.getDrawerController()!.openDrawerSide(
+            MMDrawerSide.Right, animated: true, completion: nil)
     }
     
-    
+    //open left(setting) menu
     @IBAction func openSettingMenu(sender: AnyObject) {
-        self.getDrawerController()!.openDrawerSide(MMDrawerSide.Left,
-                                                    animated: true, completion: nil)
+        self.getDrawerController()!.openDrawerSide(
+            MMDrawerSide.Left, animated: true, completion: nil)
     }
     
+    //play background music
     func playMusic() {
         self.backgroundMusicPlayer.prepareToPlay()
-        self.backgroundMusicPlayer.volume = 0
+        self.backgroundMusicPlayer.volume = Volumn.initial
+        //-1 means loop forever
         self.backgroundMusicPlayer.numberOfLoops = -1
         self.backgroundMusicPlayer.play()
         self.fadeInMusic()
     }
     
+    //fade out background music
     func fadeOutMusic() {
         if self.backgroundMusicPlayer.volume > Volumn.min {
-            self.backgroundMusicPlayer.volume = self.backgroundMusicPlayer.volume - Volumn.fadeStep
+            self.backgroundMusicPlayer.volume =
+                self.backgroundMusicPlayer.volume - Volumn.fadeStep
             
-            var dispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(Volumn.fadeStep) * Double(NSEC_PER_SEC)))
+            var dispatchTime: dispatch_time_t =
+                dispatch_time(
+                    DISPATCH_TIME_NOW,
+                    Int64(Double(Volumn.fadeStep) * Double(NSEC_PER_SEC))
+                )
+            
             dispatch_after(dispatchTime, dispatch_get_main_queue(), {
                 self.fadeOutMusic()
             })
             
         } else {
-            self.backgroundMusicPlayer.volume = self.backgroundMusicPlayer.volume - Volumn.fadeStep
+            self.backgroundMusicPlayer.volume =
+                self.backgroundMusicPlayer.volume - Volumn.fadeStep
             self.backgroundMusicPlayer.pause()
         }
     }
     
+    //fade in background music
     func fadeInMusic() {
         if self.backgroundMusicPlayer.volume < Volumn.max {
-            self.backgroundMusicPlayer.volume = self.backgroundMusicPlayer.volume + Volumn.fadeStep
+            self.backgroundMusicPlayer.volume =
+                self.backgroundMusicPlayer.volume + Volumn.fadeStep
             
-            var dispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(Volumn.fadeStep) * Double(NSEC_PER_SEC)))
+            var dispatchTime: dispatch_time_t =
+                dispatch_time(
+                    DISPATCH_TIME_NOW,
+                    Int64(Double(Volumn.fadeStep) * Double(NSEC_PER_SEC))
+                )
             dispatch_after(dispatchTime, dispatch_get_main_queue(), {
                 self.fadeInMusic()
             })
         }
     }
     
+    
+    //MARK: - Notification Handlers
     func startPlayingMusic(notification: NSNotification) {
         self.playMusic()
     }
