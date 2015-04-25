a4be711a15a77d240a71a966890fe7d11c039ec4
Jiang Sheng <gisonrg@gmail.com>
2015-04-25 18:39:44 +0800

    Improve testcase for game level

--- Planck/PlanckTests/GameLevelTest.swift
+++ Planck/PlanckTests/GameLevelTest.swift
@@ -23,7 +23,7 @@ class GameLevelTest: XCTestCase {
         
         XCTAssertEqual(game1 == game2, true, "Equatable not implemented correctly")
         XCTAssertEqual(game1 < game2, false, "Comparable not implemented correctly")
-        XCTAssertEqual(game1 < game3, false, "Comparable not implemented correctly")
+        XCTAssertEqual(game1 < game3, true, "Comparable not implemented correctly")
     }
     
     func testGetNextLevel() {
@@ -35,7 +35,7 @@ class GameLevelTest: XCTestCase {
         // things to be tested
         let game = GameLevel(levelName: "game1", levelIndex: 50, grid: grid, solvedGrid: grid, nodes: nodes, targetMusic: music)
         
-        XCTAssertEqual(game.indexForNextLevel, 51, "Equatable not implemented correctly")
+        XCTAssertEqual(game.indexForNextLevel, 51, "Get next level not implemented correctly")
     }
     
     func testDeepcopy() {
@@ -52,9 +52,34 @@ class GameLevelTest: XCTestCase {
         // if we change copy, original game should not change
         copy.index = 20
         
-        XCTAssertEqual(copy.index, 20, "Equatable not implemented correctly")
+        XCTAssertEqual(copy.index, 20, "Deep copy not implemented correctly")
         
-        XCTAssertNotEqual(game.index, copy.index, "Equatable not implemented correctly")
+        XCTAssertNotEqual(game.index, copy.index, "Deep copy not implemented correctly")
+    }
+    
+    func testEncodeAndDecode() {
+        // create test stub
+        let grid = GOGrid(width: 50, height: 50, andUnitLength: 1)
+        let nodes = [String: XNode]()
+        let music = XMusic()
+        
+        // things to be tested
+        let game = GameLevel(levelName: "game1", levelIndex: 50, grid: grid, solvedGrid: grid, nodes: nodes, targetMusic: music)
+        
+        let data = NSMutableData();
+        let archiver = NSKeyedArchiver(forWritingWithMutableData: data)
+        
+        archiver.encodeObject(game, forKey: "test")
+        archiver.finishEncoding()
+        
+        let unarchiver = NSKeyedUnarchiver(forReadingWithData: data)
+        let recoverObj = unarchiver.decodeObjectForKey("test")! as GameLevel
+        
+        XCTAssertEqual(recoverObj.index, game.index, "NSCoding not implemented correctly")
+        
+        XCTAssertEqual(recoverObj.name, game.name, "NSCoding not implemented correctly")
+        
+        XCTAssertEqual(recoverObj.targetMusic, game.targetMusic, "NSCoding not implemented correctly")
     }
     
 
